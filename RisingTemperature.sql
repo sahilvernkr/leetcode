89cd8b05b45ace4a15f101fdeeb95075bd69cfcd
sahil <sahilvernekar96@gmail.com>
2025-01-13 22:45:20 +0530

    okay

--- RisingTemperature.sql
+++ RisingTemperature.sql
@@ -2,4 +2,4 @@
 # Write your MySQL query statement below
 select id
 from Weather w1
-where w1.temperature > (select temperature from Weather w2 where w1.recordDate- INTERVAL 1 DAY = w2.recordDate)
+where w1.temperature > (select temperature from Weather w2 where w1.recordDate- INTERVAL 1 DAY = w2.recordDate) 
