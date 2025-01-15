320a2e87d9c5ece83f232f4d3567fd6d97ca9fd0
sahil <sahilvernekar96@gmail.com>
2025-01-15 22:30:33 +0530

    easy solution to plusone

--- PlusOne.php
+++ PlusOne.php
@@ -1,15 +1,15 @@
 <?php
-class Solution {
+class Solution
+{
 
-/**
- * @param Integer[] $digits
- * @return Integer[]
- */
-function plusOne($digits) {
-  $number = implode("",$digits) + 1;
-  $number2 = str_split($number);
-  return $number2;
-}
+  /**
+   * @param Integer[] $digits
+   * @return Integer[]
+   */
+  function plusOne($digits)
+  {
+    return str_split(bcadd(implode($digits), 1));
+  }
 }
 
 //https://leetcode.com/problems/plus-one/
