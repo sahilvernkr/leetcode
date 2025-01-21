1c63e0bd97e19eba6da1d14d0c493555db5cf44d
sahil <sahilvernekar96@gmail.com>
2025-01-21 08:52:46 +0530

    Search Insert Position

---
+++ SearchInsertPosition.php
@@ -0,0 +1,20 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer
+     */
+    function searchInsert($nums, $target) {
+        for($i=0;$i<count($nums);$i++){
+            if($nums[$i] == $target || $nums[$i] > $target){
+                return $i;
+            } 
+        }
+
+        return count($nums);
+    }
+}
+
+//https://leetcode.com/problems/search-insert-position/
