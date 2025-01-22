9da2cb15652cbe3ab6fca82ebfac228460365cf6
sahil <sahilvernekar96@gmail.com>
2025-01-22 09:46:01 +0530

    Binary addition setup

---
+++ BinaryAddition.php
@@ -0,0 +1,15 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param String $a
+     * @param String $b
+     * @return String
+     */
+    function addBinary($a, $b) {
+        
+    }
+}
+
+//https://leetcode.com/problems/add-binary/
