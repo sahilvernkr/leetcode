4e08c015ad51f53eec882b31626e6d492315aa45
sahil <sahilvernekar96@gmail.com>
2025-01-23 09:50:45 +0530

    attempt 1 - Binary addition

--- BinaryAddition.php
+++ BinaryAddition.php
@@ -8,8 +8,28 @@ class Solution {
      * @return String
      */
     function addBinary($a, $b) {
-        
+        $sum = [];
+        $ar = str_split(strrev($a));
+        $br = str_split(strrev($a));
+        $carry = 0;
+        for($i=0;$i<4;$i++){
+            $tot = (int)$ar[$i] + (int)$br[$i] + $carry;
+            $carry = 0;
+            if($tot == 0){
+             $sum[] = 0;   
+            }
+            if($tot == 1){
+             $sum[] = 1;   
+            }
+            if($tot == 2){
+             $sum[] = 0;
+             $carry = 1;   
+            }
+        }
+       return strrev(implode($sum));
     }
 }
 
 //https://leetcode.com/problems/add-binary/
+
+
