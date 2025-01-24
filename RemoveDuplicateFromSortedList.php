4899c4709c8eef028e2419e5123c0b556ff02653
sahil <sahilvernekar96@gmail.com>
2025-01-24 22:38:18 +0530

    Remove duplicate -- better solution

--- RemoveDuplicateFromSortedList.php
+++ RemoveDuplicateFromSortedList.php
@@ -1,4 +1,32 @@
 <?php
 
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
 
+    /**
+     * @param ListNode $head
+     * @return ListNode
+     */
+    function deleteDuplicates($head) {
+        $check = '';
+        foreach($head as $key => $h){
+            if($check == $h){
+            unset($key);
+            }
+            $check = $h;
+        }
+        $values = array_values($head);
+        return $values;
+    }
+}
 //https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
