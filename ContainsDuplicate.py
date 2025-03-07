e23341740c69cc39df80d7f1b4a6956cd9e661ba
sahil <sahilvernekar96@gmail.com>
2025-03-07 08:41:18 +0530

    Contains duplicate - hashset

--- ContainsDuplicate.py
+++ ContainsDuplicate.py
@@ -10,6 +10,16 @@ class Solution:
 
         return False
 
+# sol -2 
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
 
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+
+        return False
 
 # https://leetcode.com/problems/contains-duplicate/description/
