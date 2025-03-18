74400820cdc8b1c71da0a19da55b6dff13719fbd
sahil <sahilvernekar96@gmail.com>
2025-03-18 23:01:13 +0530

    Min Sorted array

--- MinSortedArray.py
+++ MinSortedArray.py
@@ -1,3 +1,12 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        min_value = min(nums)
+        min_index = nums.index(min_value)
+
+        if min_index == 0:
+            return len(nums)
+        return len(nums) - min_index
+
 
 
 
